ebd0eda1c2327cc7e45a475ee11b1899299122f2
Jordan Hayes <nosafoundation@gmail.com>
2020-11-22 21:42:23 -0500

    commit tree 346a655123e86bb7ac487326c494e0d6fa3ee5be

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,11 @@
 def using_while
   levitation_force = 6
-	
+	counter = 0 
+	while counter < 4
+	  puts "Wingardium Leviosa"
+	  levitation_force += 1
+	  counter += 1
+	 end
   #your code here
 end
 
